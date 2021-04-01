5c136719fa67f912e1db6e8c9848c36fb55ceff3
ng0cth1nh <thinhmaxdz@gmail.com>
2021-04-01 19:28:28 +0700

    load employee to record attendance table

---
+++ TimekeepingSystem/Assets/libs/js/recordAttend.js
@@ -0,0 +1,81 @@
+﻿window.onload = function () {
+    loadTableEmployee();
+    // submitAttendance();
+}
+
+function loadTableEmployee() {
+    $.ajax({
+        url: '/RecordAttendance/LoadDataEmployee',
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (response.status) {
+                var data = response.data;
+                var html = '';
+                var template = $('#model-employee').html();
+                $.each(data, function (i, item) {
+                    if ((i + 1) % 2 === 0) {
+                        html += Mustache.render(template, {
+                            ID: item.ID,
+                            Name: item.Name,
+                            Role: item.Role,
+                            Type: item.Type,
+                            Class: "even",
+                        });
+                    } else {
+                        html += Mustache.render(template, {
+                            ID: item.ID,
+                            Name: item.Name,
+                            Role: item.Role,
+                            Type: item.Type,
+                            Class: "odd",
+                        });
+                    }
+
+                });
+                $('#tbEmployees').html(html);
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+
+function submitAttendance (){
+    let employees = $(".employee");
+    let arr = [];
+    for (let i = 0; i < employees.length; i++) {
+        let id = $(`.employee:eq(${i}) td:eq(0)`).text();
+        let ispresent = $(`input[name=${id}]:checked`).val();
+        let note = $(`input[name=absentNode${id}]`).val();
+        let record = {
+            EmployeeID: id,
+            IsPresent: ispresent,
+            Note: note
+        }
+        arr.push(record);
+    }
+
+    $.ajax({
+        url: '/RecordAttendance/SaveRecordAttendance',
+        data: {
+            record: JSON.stringify(arr)
+        },
+        type: 'POST',
+        dataType: 'json',
+        success: function (response) {
+            if (response.status == true) {
+                $("#btnSubmit").text("Update");
+                alert("Submit Successfully!");   
+            } else {
+                alert(response.message);
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+
+}
--- TimekeepingSystem/Controllers/RecordAttendanceController.cs
+++ TimekeepingSystem/Controllers/RecordAttendanceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,41 @@ namespace TimekeepingSystem.Controllers
 {
     public class RecordAttendanceController : Controller
     {
+        private TimekeepingDbContext context;
+
+        public RecordAttendanceController()
+        {
+            context = new TimekeepingDbContext();
+        }
+
         // GET: RecordAttendance
-     
+        
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult LoadDataEmployee()
+        {
+            context.Configuration.ProxyCreationEnabled = false;
+            var listEmployees = from e in context.Employees
+                                join er in context.EmployeeRoles on e.RoleID equals er.ID
+                                join et in context.EmployeeTypes on e.TypeID equals et.ID
+                                select new
+                                {
+                                    ID = e.ID,
+                                    Name = e.Name,
+                                    Role = er.Name,
+                                    Type = et.Name,
+                                };
+            return Json(new
+            {
+                data = listEmployees,
+                status = true,
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
